45354d67f2fb59af0d06be3c3e36585cc1d70c4f
Andrew Drebot <andriy12345678910@gmail.com>
2021-11-01 22:11:09 +0200

    Changing the name of the scheme and collection

--- Backend/src/main/java/andrew/project/ToDoList/api/entities/ToDoDbi.java
+++ Backend/src/main/java/andrew/project/ToDoList/api/entities/ToDoDbi.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "to_do")
+@Document(collection = "to_do_list")
 public class ToDoDbi {
     @Id
     String id;
